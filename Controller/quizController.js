be8a363d7f7cd5730930e5ead862eec7b6d55401
erramifadelellah && ayman eljamaaoui <fadelellaherrami@gmail.com>
2023-01-01 11:50:08 +0100

    third commit

--- Controller/quizController.js
+++ Controller/quizController.js
@@ -4,9 +4,9 @@ const quizModel = require('../models/QuizModel');
 //Ajouter une question
 const AjouteQuiz = async (req, res) => {
     // Récupérez la liste des questions créées par l'utilisateur ici
-    const {name,startDate,endDate,students,attemptsAllowed,attempts,questions} = req.body;
-    console.log({name,startDate,endDate,students,attemptsAllowed,attempts,questions});
-    const newQuiz = await quizModel.create({name,startDate,endDate,students,attemptsAllowed,attempts,questions});
+    const {name,startDate,endDate,utilisateur,attemptsAllowed,attempts,questions} = req.body;
+    console.log({name,startDate,endDate,utilisateur,attemptsAllowed,attempts,questions});
+    const newQuiz = await quizModel.create({name,startDate,endDate,utilisateur,attemptsAllowed,attempts,questions});
     res.json(newQuiz);
   };
 // Récuperer tout les questions
@@ -41,8 +41,8 @@ const supprimerUnQuiz = async (req,res)=>{
 // modifier une question
 const updateQuiz = async (req,res)=>{
     const quizId = req.params;
-    const {name,startDate,endDate,students,attemptsAllowed,attempts,questions} = req.body;
-    const updatedQuiz = await quizModel.findByIdAndUpdate(quizId,{name,startDate,endDate,students,attemptsAllowed,attempts,questions});
+    const {name,startDate,endDate,utilisateur,attemptsAllowed,attempts,questions} = req.body;
+    const updatedQuiz = await quizModel.findByIdAndUpdate(quizId,{name,startDate,endDate,utilisateur,attemptsAllowed,attempts,questions});
     res.json(updatedQuiz);
 };
 
